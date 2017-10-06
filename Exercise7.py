7a127ea3f847a704773a6e5b1c107aa56cb9ca6d
Grant Keller <gkeller1@nd.edu>
2017-10-06 15:47:55 -0400

    Including data, began working on Q2.

--- Exercise7.py
+++ Exercise7.py
@@ -9,6 +9,50 @@
 #Q2
 #Pseudocode
 
+"""
+1. load in data (open file or straight to dataframe?)
+    Data contains headers, not as separate row:
+        format: HEADER: DATA HEADER: DATA...
+        63 positions * 20 amino acids * 10 decoys = 12,600 lines
+        Waaaay too many. Gonna snag a single position (200).
+        Plot BindingScore vs. Solvation energy?
+2. 
+"""
+# open file
+# for each line
+#   split line by ' '
+#   headers
+#   append info
+
+inFile = open('HCV1406_scan.fasc', 'r')
+
+filename = []
+total_score = []
+fa_atr = []
+fa_rep = []
+fa_sol = []
+fa_intra_rep = []
+fa_elec = []
+pro_close = []
+hbond_sr_bb = []
+hbond_lr_bb = []
+hbond_bb_sc = []
+hbond_sc = []
+dslf_fa13 = []
+rama = []
+omega = []
+fa_dun = []
+p_aa_pp = []
+yhh_planarity = []
+ref = []
+TotalScore = []
+HLAScore = []
+TCRScore = []
+BindingScore = []
+LoopRMSD = []
+for line in inFile:
+    line = line.strip().split(' ')
+    
 
 
 #Q3
